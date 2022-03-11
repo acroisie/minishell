470b689197c215dda5b799cc6dd066875937064f
arnaud <arnaud.croisier@live.fr>
2022-03-11 08:43:35 +0100

    save

--- srcs/parse_args_utils.c
+++ srcs/parse_args_utils.c
@@ -6,9 +6,31 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 15:53:16 by acroisie          #+#    #+#             */
-/*   Updated: 2022/03/10 18:13:13 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/03/11 08:25:27 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+char	*ft_add_char(char *s1, char c)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+		return (ft_strdup((char *)c));
+	if (!c)
+		return (ft_strdup((char *)s1));
+	str = malloc((ft_strlen(s1) + 2) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	while (s1[j])
+		str[i++] = s1[j++];
+	str[i++] = c;
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
