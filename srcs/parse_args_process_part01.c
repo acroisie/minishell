e73cafa170906c36eb1f970c85b7dfb21e65d392
arnaud <arnaud.croisier@live.fr>
2022-03-22 17:16:50 +0100

    add_env_semi_functional

--- srcs/parse_args_process_part01.c
+++ srcs/parse_args_process_part01.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 14:26:56 by acroisie          #+#    #+#             */
-/*   Updated: 2022/03/21 14:17:48 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/03/22 16:27:00 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,11 @@ void	ft_pipe_process(char *line, t_var *var)
 
 void	ft_space_process(char *line, t_var *var)
 {
-	var->j++;
+	if (var->lst_cmd->args[var->j][0] != '\0')
+		var->j++;
 	while (line[var->i] == ' ')
 		var->i++;
-	if (line[var->i] == '|')
-		ft_pipe_process(line, var);
-	else if (line[var->i] == '\0')
+	if (line[var->i] == '\0' || line[var->i] == '|')
 		return ;
 	else
 	{	
@@ -45,7 +44,7 @@ void	ft_s_quotes_process(char *line, t_var *var)
 	var->i++;
 	while (line[var->i] != '\'')
 	{
-		if (line[var->i] == '\0') // Modify when Luther's back.
+		if (line[var->i] == '\0') // Modify to make a clean end
 		{
 			printf("unclosed quotes\n");
 			exit (1);
@@ -64,7 +63,7 @@ void	ft_d_quotes_process(char *line, t_var *var, char **env)
 	{
 		if (line[var->i] == '$')
 			ft_dollar_sign_process(line, var, env);
-		else if (line[var->i] == '\0') // Modify when Luther's back.
+		else if (line[var->i] == '\0') // Modify to make a clean end
 		{
 			printf("unclosed quotes\n");
 			exit (1);
@@ -88,7 +87,13 @@ void	ft_dollar_sign_process(char *line, t_var *var, char **env)
 	var->i++;
 	mem = var->i;
 	k = 0;
-	while (ft_isalnum(line[var->i]) || line[var->j] == '_')
+	if (line[var->i] == ' ' || line[var->i] == '\0' || line[var->i] == '|') 
+	{
+		var->lst_cmd->args[var->j] = ft_add_char(
+				var->lst_cmd->args[var->j], '$');
+		return ;
+	}
+	while (ft_isalnum(line[var->i]) || line[var->i] == '_')
 		var->i++;
 	temp = ft_strndup(&line[mem], (var->i - mem));
 	while (env[k])
