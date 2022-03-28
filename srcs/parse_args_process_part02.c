25ec9de87fb57b533057703c4f254a0b9a112b92
acroisie <acroisie@student.42lyon.fr>
2022-03-28 11:02:55 +0200

    save_28-03

--- srcs/parse_args_process_part02.c
+++ srcs/parse_args_process_part02.c
@@ -6,7 +6,7 @@
 /*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/16 09:34:41 by acroisie          #+#    #+#             */
-/*   Updated: 2022/03/24 15:11:27 by acroisie         ###   ########lyon.fr   */
+/*   Updated: 2022/03/28 10:11:44 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,6 @@ void	ft_tilde_process(t_var *var, char **env)
 		{
 			if (env[k][4] == '=')
 			{
-				// var->lst_cmd->args[var->j] = ft_strjoin_free_s1(
-				// 		var->lst_cmd->args[var->j],
-				// 		&env[k][5]);
 				ft_write_string_output(k, var, env);
 				return ;
 			}
@@ -44,32 +41,32 @@ void	ft_tilde_process(t_var *var, char **env)
 
 void	ft_redir_process(char *line, t_var *var, char **env)
 {
-	char	mem;
+	// char	mem;
 
-	if (line[var->i] == '<')
-		mem = 1;
-	else
-		mem = 2;
-	var->i++;
-	if (line[var->i] == line[var->i - 1])
-	{
+	// if (line[var->i] == '<')
+	// 	mem = 1;
+	// else
+	// 	mem = 2;
+	// var->i++;
+	// if (line[var->i] == line[var->i - 1])
+	// {
 		
-	}
-		if (mem == 1)
-			printf("Heredoc\n");    // To delete
-		else
-			printf("Add line to\n");    // To delete
-	while (line[var->i == ' '])
-		var->i++;
-	while (line[var->i] != ' ' || line[var->i] != '\0' || line[var->i] != '|')
-	{
-		if (line[var->i] == '\'')
-			ft_s_quotes_process(line, var);
-		else if (line[var->i] == '"')
-			ft_d_quotes_process(line, var, env);
-		else if (line[var->i] == '$')
-			ft_dol_sign_process(line, var, env, 1);
-		else if (line[var->i] == '~')
-			ft_tilde_process(var, env);
-	}
+	// }
+	// 	if (mem == 1)
+	// 		printf("Heredoc\n");    // To delete
+	// 	else
+	// 		printf("Add line to\n");    // To delete
+	// while (line[var->i == ' '])
+	// 	var->i++;
+	// while (line[var->i] != ' ' || line[var->i] != '\0' || line[var->i] != '|')
+	// {
+	// 	if (line[var->i] == '\'')
+	// 		ft_s_quotes_process(line, var);
+	// 	else if (line[var->i] == '"')
+	// 		ft_d_quotes_process(line, var, env);
+	// 	else if (line[var->i] == '$')
+	// 		ft_dol_sign_process(line, var, env, 1);
+	// 	else if (line[var->i] == '~')
+	// 		ft_tilde_process(var, env);
+	// }
 }
