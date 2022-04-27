435bd6c82a8f0ddba4596735f8c5785672859052
arnaud <arnaud.croisier@live.fr>
2022-04-27 09:56:25 +0200

    update_exit

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lnemor <lnemor@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: acroisie <acroisie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 18:57:55 by lnemor            #+#    #+#             */
-/*   Updated: 2022/04/26 19:00:56 by lnemor           ###   ########lyon.fr   */
+/*   Updated: 2022/04/27 09:51:08 by acroisie         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,25 +35,24 @@ void	ft_exit(t_lst_cmd *lst_cmd)
 	int				j;
 
 	i = 0;
-	j = -1;
+	j = 0;
 	while (lst_cmd->args[++i])
 	{
 		if (i > 1)
-		{
-			g_rvalue = 1;
-			return (ft_putendl_fd("minishell: exit: too many arguments", 2));
-		}
-		if (lst_cmd->args[i][0] == '-' || lst_cmd->args[i][0] == '+')
+			return (return_error_exit("exit: ", "", " too many arguments", 1));
+		while (lst_cmd->args[i][j] == ' ')
 			j++;
-		while (lst_cmd->args[i][++j])
+		if (lst_cmd->args[i][j] == '-' || lst_cmd->args[i][j] == '+')
+			j++;
+		while (lst_cmd->args[i][j])
+		{
 			if (ft_isdigit(lst_cmd->args[i][j]) == 0)
 				return_error_exit("exit: ", lst_cmd->args[1],
 					": numeric argument required", 255);
+			j++;
+		}
 	}
-	if (i <= 2)
-	{
-		if (lst_cmd->args[1])
-			the_noar_exit(ft_atol(lst_cmd->args[1]), lst_cmd);
-		the_noar_exit(g_rvalue, lst_cmd);
-	}
+	if (lst_cmd->args[1])
+		the_noar_exit(ft_atol(lst_cmd->args[1]), lst_cmd);
+	the_noar_exit(g_rvalue, lst_cmd);
 }
